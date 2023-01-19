7aeed3897cf63c23a378db758fc5704ce84c0b86
Chevyself <chevyself@gmail.com>
2023-01-19 17:10:39 +0000

    Deploying to gh-pages from  @ aed05af0c032fb45b63907a2ee51eb96ccfe851d 🚀

---
+++ _app/immutable/chunks/1-1972edf3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-dd7d2e21.js";export{t as component};
---
+++ _app/immutable/chunks/2-94194f9e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2a739914.js";export{t as component};
