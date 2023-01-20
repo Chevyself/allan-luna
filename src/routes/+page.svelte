341b45d47c4b6f26331db9f3a9706bc86579de6d
Chevyself <chevyself@gmail.com>
2023-01-19 19:13:54 -0500

    Prototype is ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,78 @@
+<script lang="ts">
+    import Work from "../components/Work.svelte";
+
+    let works = [
+        {
+            name: "1",
+            image: "images/1-1.jpg",
+        },
+        {
+            name: "2",
+            image: "images/2-2.jpg",
+            full: true,
+            classes: "full-view",
+            shadow: true,
+        },
+        {
+            name: "3",
+            image: "images/3-1.jpg",
+            left: false
+        },
+        {
+            name: "4",
+            image: "images/4.jpg",
+        },
+        {
+            name: "5",
+            image: "images/5.jpg",
+            left: false
+        },
+        {
+            name: "6",
+            image: "images/6.jpg",
+        }
+    ];
+</script>
+
 <svelte:head>
     <title>Allan Luna</title>
 </svelte:head>
 
-<script lang="ts">
-    import Work from '../components/Work.svelte';
-
-    let works = [{
-        name: '1',
-        description: 'Test',
-        image: '/images/ph-1.jfif'
-    },
-    {
-        name: '2',
-        description: 'Test'
-    },
-    {
-        name: '3',
-        description: 'Test',
-        image: '/images/ph-2.jpg'
-    }];
-</script>
+<div class="view-parent">
+    <section id="landing" class="view">
+        <div id="introduction" class="container-fluid h-100 p-5">
+            <div class="row h-100">
+                <div
+                    class="col-12 col-md-6 offset-md-6 d-flex flex-column justify-content-center"
+                >
+                    <h1>Retratos</h1>
+                    <h4 class="display-4">Allan Luna</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="works">
+        {#each works as work}
+            <Work
+                name={work.name}
+                description={work.description}
+                image={work.image}
+                classes={work.classes}
+                full={work.full}
+                shadow={work.shadow}
+                left={work.left}
+            />
+        {/each}
+    </section>
+</div>
 
 <style lang="scss">
-    @import '../styles/sections.scss';
+    @import "../styles/sections.scss";
     // Make #landing content padding responsive and bigger in bigger displays
     #landing {
         padding: 15rem 0;
         height: 100vh;
-        background-image: url('/images/allan.png');
+        background-image: url("/images/allan.png");
         background-repeat: no-repeat;
         // Position it to the left bottom
         background-position: left bottom;
@@ -43,33 +88,33 @@
         animation: fadeInUp 1.7s;
     }
 
-    
-  @keyframes fadeInUp {
-    from {
-      opacity: 0;
-      transform: translate3d(0, 40px, 0);
+    @keyframes fadeInUp {
+        from {
+            opacity: 0;
+            transform: translate3d(0, 40px, 0);
+        }
+        to {
+            opacity: 1;
+            transform: none;
+        }
     }
-    to {
-      opacity: 1;
-      transform: none;
-    }
-  }
-</style>
 
-<div class="view-parent">
-    <section id="landing" class="view">
-        <div id="introduction" class="container-fluid h-100 p-5">
-            <div class="row h-100">
-                <div class="col-12 col-md-6 offset-md-6 d-flex flex-column justify-content-center">
-                    <h1 class="display-1">Retratos</h1>
-                    <h4>Allan Luna</h4>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="works">
-        {#each works as work}
-            <Work name={work.name} description={work.description} full={true} image={work.image}/>
-        {/each}
-    </section>
-</div>
+    .artwork-wrapper {
+        min-height: 100vh;
+    }
+
+    .artwork {
+        // Make it small
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        img {
+            width: 70%;
+        }
+    }
+
+    .artwork-text {
+        margin: auto;
+        padding: 2rem;
+        width: 100%;
+    }
+</style>
